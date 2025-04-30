62717ccce49506d5f3e3df94d1d48866bde29225
Minter0817 <adf5858@naver.com>
2025-04-30 15:06:17 +0900

    Feat: FindPw View 구현

--- SmartCampus/Features/Auth/Find/FindPw.swift
+++ SmartCampus/Features/Auth/Find/FindPw.swift
@@ -18,9 +18,37 @@ struct FindPw: View {
                 NavButton(lable:"아이디 찾기")
                 NavButton(lable:"비밀번호 찾기", isSelected: true)
             }
+            .padding(.bottom, 46)
             
+            //아이디 입력 필드
+            Field(text:"아이디 입력")
+                .padding(.horizontal, 30)
+                .padding(.vertical, 5)
+            
+            //휴대폰 번호 입력 필드 + 버튼
+            HStack(alignment: .center, spacing: 13) {
+                Field(text: "휴대폰 번호 입력('-'제외)")
+                BlueStrokeBotton(text: "인증번호 전송")
+                }
+            .padding(.horizontal, 30)
+            .padding(.vertical, 5)
+            
+            //인증번호 입력 필드 + 버튼
+            HStack(alignment: .center, spacing: 13) {
+                Field(text: "인증번호 입력")
+                BlueStrokeBotton(text:"확인")
+            }
+            .padding(.horizontal, 30)
+            .padding(.vertical, 5)
+            .padding(.bottom, 37)
+            
+            
+            
+            //하단 비밀번호 찾기 버튼
             BlueButton(text:"비밀번호 찾기")
+                .padding(.horizontal,10)
         }
+        Spacer()
     }
 }
 
