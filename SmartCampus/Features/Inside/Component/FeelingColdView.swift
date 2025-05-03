5e1b5eee4db05edf56ec76f5667218d0c90a1cd2
Minter0817 <adf5858@naver.com>
2025-05-04 00:02:12 +0900

    Feat(rsc): 추워요 학생 아이콘 추각

---
+++ SmartCampus/Features/Inside/Component/FeelingColdView.swift
@@ -0,0 +1,23 @@
+//
+//  StatusButton.swift
+//  SmartCampus
+//
+//  Created by 이민지 on 5/3/25.
+//
+import SwiftUI
+
+struct FeelingVoteButtonView: View {
+    
+    
+    
+    var body: some View {
+        HStack(alignment:.center, spacing:0) {
+            Image("person.cold")
+        }
+    }
+}
+
+
+#Preview() {
+    FeelingVoteButtonView()
+}
---
+++ SmartCampus/Features/Inside/Component/WhiteFrame.swift
@@ -0,0 +1,18 @@
+//
+//  WhiteFrame.swift
+//  SmartCampus
+//
+//  Created by 이민지 on 5/3/25.
+//
+
+import SwiftUI
+
+struct WhiteFrame: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WhiteFrame()
+}
