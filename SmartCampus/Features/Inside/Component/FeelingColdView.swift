8ab346bcc2eb4c4129064b4df76937d3ce4ecbf0
Minter0817 <adf5858@naver.com>
2025-05-04 00:20:32 +0900

    Feat(component) : 상태(추워요/더워요) 선택 컴포넌트 구현

--- SmartCampus/Features/Inside/Component/FeelingColdView.swift
+++ SmartCampus/Features/Inside/Component/FeelingColdView.swift
@@ -6,18 +6,46 @@
 //
 import SwiftUI
 
-struct FeelingVoteButtonView: View {
-    
-    
+struct FeelingColdView: View {
+    var isSelected: Bool = true
     
     var body: some View {
+        //전체 뷰
         HStack(alignment:.center, spacing:0) {
+            
+            //이미지뷰
             Image("person.cold")
+                .padding(.trailing, 12)
+            
+            Text("추워요!")
+                .font(
+                    Font.custom("Pretendard", size: 16)
+                    .weight(.medium)
+                )
         }
+        .padding(.leading, 35)
+        .padding(.trailing, 43)
+        .frame(maxWidth: .infinity, minHeight: 76, alignment: .leading)
+        .background(.white)
+        .cornerRadius(10)
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(isSelected
+                        ? Color(red: 0.06, green: 0.59, blue: 0.72)
+                        : Color.clear, //stroke 없음
+                        lineWidth: 1
+                       )
+        )
+        
+        
+        
+        
+        
+        
     }
 }
 
 
 #Preview() {
-    FeelingVoteButtonView()
+    FeelingColdView()
 }
